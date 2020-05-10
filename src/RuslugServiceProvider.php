298f1ca89adfe1ce804df48aa7f82f14d1694865
Зограбян Симон <simon.zograbyan@brainysoft.ru>
2020-05-10 14:06:36 +0300

    Initial commit

---
+++ src/RuslugServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace Rolexam\Ruslug;
+
+use Illuminate\Support\ServiceProvider;
+
+class RuslugServiceProvider extends ServiceProvider{
+
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('slug', function($app){
+            return new Slug();
+        });
+	}
+
+}
